e7d63a62122157f14da9815fb6195016e125d900
restlessmedia <phillycooper@hotmail.com>
2019-04-05 10:26:24 +0100

    Added model & test

---
+++ restlessmedia.Module.Contact.UnitTest/ContactModelTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace restlessmedia.Module.Contact.UnitTest
+{
+  [TestClass]
+  public class ContactModelTests
+  {
+    [TestMethod]
+    public void DataProtection_sets_ContactFlags()
+    {
+      new ContactModel
+      {
+        DataProtection = true,
+      }.ContactFlags.HasFlag(ContactFlags.DataProtection).MustBeTrue();
+
+      new ContactModel
+      {
+        DataProtection = false,
+      }.ContactFlags.HasFlag(ContactFlags.DataProtection).MustBeFalse();
+    }
+  }
+}
---
+++ src/ContactViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.DataAnnotations;
+using restlessmedia.Module.DataAnnotations;
+
+namespace restlessmedia.Module.Contact
+{
+  public class ContactModel : EnquiryEntity
+  {
+    public ContactModel() { }
+
+    [Required(ErrorMessage = "Name is required")]
+    [DataType(DataType.Text)]
+    public override string Name
+    {
+      get
+      {
+        return base.Name;
+      }
+      set
+      {
+        base.Name = value;
+      }
+    }
+
+    [Required(ErrorMessage = "Email is required")]
+    [Email(ErrorMessage = "Invalid email")]
+    [DataType(DataType.EmailAddress)]
+    public override string Email
+    {
+      get
+      {
+        return base.Email;
+      }
+      set
+      {
+        base.Email = value;
+      }
+    }
+
+    [TelephoneNumber(ErrorMessage = "Invalid")]
+    [DataType(DataType.Text)]
+    public override string ContactNumber
+    {
+      get
+      {
+        return base.ContactNumber;
+      }
+      set
+      {
+        base.ContactNumber = value;
+      }
+    }
+
+    public bool DataProtection
+    {
+      get
+      {
+        return ContactFlags.HasFlag(ContactFlags.DataProtection);
+      }
+      set
+      {
+        if (value)
+        {
+          ContactFlags |= ContactFlags.DataProtection;
+        }
+        else
+        {
+          ContactFlags &= ~ContactFlags.DataProtection;
+        }
+      }
+    }
+  }
+}
